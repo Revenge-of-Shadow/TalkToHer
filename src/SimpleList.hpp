ee80320d501737a27c468fe476970871ab6c5f79
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2025-11-16 21:46:28 +0100

    0B: Classic. Template array class for handling simple class arrays slowly but sparingly.

---
+++ src/SimpleList.hpp
@@ -0,0 +1,41 @@
+template <typename T> class SimpleList {
+  int size;
+  T *elements;
+
+public:
+  SimpleList() : size(0), elements(nullptr) {}
+  ~SimpleList() { delete[] elements; }
+
+  int getSize() { return size; }
+
+  int add(T element) {
+    T *newElements = new T[size + 1];
+    for (int i = 0; i < size; ++i) {
+      newElements[i] = elements[i];
+    }
+    newElements[size] = element;
+    delete[] elements;
+    elements = newElements;
+
+    return ++size;
+  }
+
+  T peek(int index) { return elements[index]; }
+  T pop(int index) {
+    T popped;
+    T *newElements = new T[size - 1];
+    for (int i = 0; i < size; ++i) {
+      if (i == index)
+        popped = elements[i];
+      else if (i > index)
+        newElements[i - 1] = elements[i];
+      else
+        newElements[i] = elements[i];
+    }
+    delete[] elements;
+    elements = newElements;
+    --size;
+
+    return popped;
+  }
+};
